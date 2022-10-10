20c9bfa9fb9265df889c7bbacc5c64e9b50fe8ee
Ivan Popov <85757205+ivanpopov2002@users.noreply.github.com>
2022-10-10 20:12:40 +0300

    Update the_number_of_positives_on_the_segment.py

--- Ideas and algorithms/prefix sums/the_number_of_positives_on_the_segment.py
+++ Ideas and algorithms/prefix sums/the_number_of_positives_on_the_segment.py
@@ -1,1 +1,15 @@
+n = int(input())
+numbers = list(map(int, input().split()))
 
+prPos = [0]
+
+for i in range(n):
+    if numbers[i] > 0:
+        prPos.append(prPos[-1] + 1)
+    else:
+        prPos.append(prPos[-1])
+
+q = int(input())
+for i in range(q):
+    l, r = (int(i) for i in input().split())
+    print(prPos[r] - prPos[l - 1])
